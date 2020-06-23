407cf8c3a3e49fb2ebdf90bece4b736cb6693901
joegturner <59180847+joegturner@users.noreply.github.com>
2020-06-23 13:21:24 -0400

    added fetch request to random quote API

--- js/script.js
+++ js/script.js
@@ -7,145 +7,176 @@ project 1 - A Random Quote Generator
 //First submission failed, resubmitting
 //Fixed 'quote' array to 'quotes'
 
-// 'quotes` array 
-var quotes = [
+// 'quotes` array
+let quotes = [];
+
+const backupQuotes = [
   {
-    quote: 'Hakuna matata', 
-    source: 'Simon', 
-    citation: 'Lion King',
+    text: "Hakuna matata",
+    author: "Simon",
+    citation: "Lion King",
     year: 1994,
-    category: 'motivational',
-    rating: 6.2
+    category: "motivational",
+    rating: 6.2,
   },
-  { 
-    quote: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.', 
-    source: 'Helen Keller', 
-    citation: '',
+  {
+    text:
+      "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+    author: "Helen Keller",
+    citation: "",
     year: null,
-    category: 'inspirational',
-    rating: 6.5
+    category: "inspirational",
+    rating: 6.5,
   },
-  { 
-    quote: 'Do. Or do not. There is no try.',
-    source: 'Yoda', 
-    citation: 'Star Wars: The Empire Strikes Back',
+  {
+    text: "Do. Or do not. There is no try.",
+    author: "Yoda",
+    citation: "Star Wars: The Empire Strikes Back",
     year: 1980,
-    category: 'motivational',
-    rating: 10
+    category: "motivational",
+    rating: 10,
   },
-  { 
-    quote: 'Always.',
-    source: 'Severus Snape', 
-    citation: 'Harry Potter: The Deathly Hollows',
+  {
+    text: "Always.",
+    author: "Severus Snape",
+    citation: "Harry Potter: The Deathly Hollows",
     year: 2007,
-    category: 'epic',
-    rating: 8.1
+    category: "epic",
+    rating: 8.1,
   },
-  { 
-    quote: 'Falling down is how we grow. Staying down is how we die.',
-    source: 'Maximus', 
-    citation: 'Gladiator',
+  {
+    text: "Falling down is how we grow. Staying down is how we die.",
+    author: "Maximus",
+    citation: "Gladiator",
     year: 2000,
-    category: 'motivational',
-    rating: 9.4
+    category: "motivational",
+    rating: 9.4,
   },
-  { 
-    quote: 'Yeah, well - the Dude abides.',
-    source: 'The Dude', 
-    citation: 'The Big Lebowski',
+  {
+    text: "Yeah, well - the Dude abides.",
+    author: "The Dude",
+    citation: "The Big Lebowski",
     year: 1998,
-    category: 'humor',
-    rating: 9.1
-  }
+    category: "humor",
+    rating: 9.1,
+  },
 ];
-var lastRandom = 0; //previous randomly generated number
-var delay = 20000; //quote refresh delay
-var interval; //setInterval() timer
+
+let fetchedQuotes = [];
+
+let lastRandom = 0; //previous randomly generated number
+const delay = 20000; //quote refresh delay
+let interval; //setInterval() timer
 
 // 'print' function
 function print(message) {
-  var outputDiv = document.getElementById('quote-box');
+  const outputDiv = document.getElementById("quote-box");
   outputDiv.innerHTML = message;
 }
 
 /* 'getRandomQuote` function
-** Generates a random number
-** Returns an Object from the 'quote' array
-** New 'random' is compared against 'lastRandom' in order to avoid quote repeats
-*/
-function getRandomQuote(){
-  var random = Math.floor(Math.random()*quotes.length);
-  var quoteObj = {};
-  do{
-    random = Math.floor(Math.random()*quotes.length);
-   } while (random === lastRandom)
+ ** Generates a random number
+ ** Returns an Object from the 'quote' array
+ ** New 'random' is compared against 'lastRandom' in order to avoid quote repeats
+ */
+function getRandomQuote() {
+  let random = Math.floor(Math.random() * quotes.length);
+  let quoteObj = {};
+  do {
+    random = Math.floor(Math.random() * quotes.length);
+  } while (random === lastRandom);
   quoteObj = quotes[random];
   lastRandom = random;
   return quoteObj;
-};
+}
 
 /* 'printQuote` function
-** Concatenates random quote and prints by calling 'print' function
-*/
-function printQuote(){
-  var message='';
+ ** Concatenates random quote and prints by calling 'print' function
+ */
+function printQuote() {
+  var message = "";
   var quoteObj = getRandomQuote();
 
   // builds HTML for 'quote', 'source', 'citation', and 'year'
-  message += '<p class="quote">' + quoteObj.quote + '</p>';
-  message += '<p class="source">' + quoteObj.source;
-  if(quoteObj.citation){
-    message += '<span class="citation">' + quoteObj.citation + '</span>';
+  message += '<p class="quote">' + quoteObj.text + "</p>";
+  message += '<p class="source">' + quoteObj.author;
+  if (quoteObj.citation) {
+    message += '<span class="citation">' + quoteObj.citation + "</span>";
   }
-  if(quoteObj.year){
-    message += '<span class="year">' + quoteObj.year + '</span>';
+  if (quoteObj.year) {
+    message += '<span class="year">' + quoteObj.year + "</span>";
   }
-  message += '</p>';
+  message += "</p>";
 
   // builds HTML for 'category' and 'rating' properties
-  if(quoteObj.category || quoteObj.rating){
+  if (quoteObj.category || quoteObj.rating) {
     message += '<p class="source">';
-    if(quoteObj.category){
+    if (quoteObj.category) {
       message += 'Category: "' + quoteObj.category + '"';
     }
-    if(quoteObj.rating){
-      message += ' / User Rating: ' + quoteObj.rating;
+    if (quoteObj.rating) {
+      message += " / User Rating: " + quoteObj.rating;
     }
-    message += '</p>';
+    message += "</p>";
   }
 
   setBackgroundColor(randomColor()); //change background color
   autoRefreshQuote(); //start page refresh timer
   print(message); //print random quote
-};
+}
 
 /* 'setBackgroundColor' function
-** Changes background color based on hexadecimal
-*/
-function setBackgroundColor(hexColor){
-  document.body.style.backgroundColor = '#' + hexColor;
+ ** Changes background color based on hexadecimal
+ */
+function setBackgroundColor(hexColor) {
+  document.body.style.backgroundColor = "#" + hexColor;
 }
 
 /* 'randomColor' function
-** Random hexadecimal is padded with 0's
-** Returns a random hexadecimal from #000000 to #FFFFFF
-*/
-function randomColor(){
-  var hexColor = '';
+ ** Random hexadecimal is padded with 0's
+ ** Returns a random hexadecimal from #000000 to #FFFFFF
+ */
+function randomColor() {
+  var hexColor = "";
   //Found the below code for generating a random color in hex. Posted by Nicolas Buduroi on https://stackoverflow.com/questions/1484506/random-color-generator
-  hexColor = (Math.random().toString(16) + '000000').substring(2, 8);
+  hexColor = (Math.random().toString(16) + "000000").substring(2, 8);
   return hexColor;
 }
 
 /* 'autoRefreshQuote' function
-**Reloads a new random quote based on 'delay' variable.
-*/
-function autoRefreshQuote(){
+ **Reloads a new random quote based on 'delay' variable.
+ */
+function autoRefreshQuote() {
   clearInterval(interval);
   interval = window.setInterval(printQuote, delay);
 }
 
+// fetch from quotes API
+const fetchQuote = () => {
+  fetch("https://type.fit/api/quotes")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      data.map((quote) => {
+        if (quote.author === null) {
+          quote.author = "unknown";
+        }
+      });
+      quotes = data;
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log(`There was an error fetching random quotes API.`);
+      console.log(`Loading backup set of quotes`);
+      quotes = backupQuotes;
+    });
+};
+
 autoRefreshQuote();
+fetchQuote();
 
 //click event listener for the print quote button
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
